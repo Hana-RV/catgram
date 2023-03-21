10d0dfb959ce34ed2c6982fd6e267a4fe6333835
Hana-RV <125707348+Hana-RV@users.noreply.github.com>
2023-03-20 19:49:54 -0500

    Delete proyecto42 directory

--- proyecto42/app/postList.js
+++
@@ -1,9 +0,0 @@
-/*const postList = document.querySelector(".posts");
-export const setupPosts = (data) => {
-
-    if (data.length){
-        postList.innerHTML = data;
-    }else{
-        postList.innerHTML = '<h4 class="text-black">Inicia sesion para ver</h4>'; 
-    }
-  };*/
